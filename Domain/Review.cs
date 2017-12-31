f0a8d087d5a07dd2ea56922385b86c43073f4c0b
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:06:41 -0800

    Adding Review model POCO

---
+++ Domain/Review.cs
@@ -0,0 +1,19 @@
+﻿using JsonApiDotNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonApiCoreBooksSample.Domain
+{
+    public class Review : Identifiable
+    {
+        [Attr("BookId")]
+        public int BookId { get; set; }
+
+        [Attr("ReviewerName")]
+        public string ReviewerName { get; set; }
+
+        [Attr("ReviewBody")]
+        public string ReviewBody { get; set; }
+    }
+}
