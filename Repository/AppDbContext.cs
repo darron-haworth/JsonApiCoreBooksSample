2377ce1e61b15a471bf5e5e5ddfa6372651ffa47
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:12:28 -0800

    Add DbContext for domain models

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JsonApiCoreBooksSample.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace JsonApiCoreBooksSample.Repository
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { } 
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
