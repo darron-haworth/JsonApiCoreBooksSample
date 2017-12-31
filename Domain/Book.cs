1992446a815f069bc1291d8e996ede24b4034f65
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:06:30 -0800

    Adding Book model POCO

---
+++ Domain/Book.cs
@@ -0,0 +1,21 @@
+﻿using JsonApiDotNetCore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace JsonApiCoreBooksSample.Domain
+{
+    public class Book : Identifiable
+    {
+        [Attr("Title")]
+        public string Title { get; set; }
+        [Attr("ISBN")]
+        public string Isbn { get; set; }
+        [Attr("PublishDate")]
+        public DateTime PublishDate { get; set; }
+
+        [Attr("Reviews")]
+        [HasMany("Reviews")]
+        public List<Review> BookReviews { get; set; }
+
+    }
+}
