6d277abec8f61a609c135b5e9939f81cd2627c51
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:18:45 -0800

    Add new Authors Conroller that Imlements JsonApiController

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonApiCoreBooksSample.Domain;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JsonApiCoreBooksSample.Controllers
+{
+    public class AuthorsController : JsonApiController<Author>
+    {
+        public AuthorsController(
+        IJsonApiContext jsonApiContext,
+        IResourceService<Author> resourceService,
+        ILoggerFactory loggerFactory)
+    : base(jsonApiContext, resourceService, loggerFactory)
+        { }
+    }
+}
