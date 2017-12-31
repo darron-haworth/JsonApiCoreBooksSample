a8445c1b6cc215854307e5a7c22d12d962686c4e
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:19:36 -0800

    Add new ReviewsController that imlements JsonApiController

---
+++ Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonApiCoreBooksSample.Domain;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JsonApiCoreBooksSample.Controllers
+{
+    public class ReviewsController : JsonApiController<Review>
+    {
+        public ReviewsController(
+        IJsonApiContext jsonApiContext,
+        IResourceService<Review> resourceService,
+        ILoggerFactory loggerFactory)
+    : base(jsonApiContext, resourceService, loggerFactory)
+        { }
+    }
+}
