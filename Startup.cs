96da8873a45e81894d05287a342abfe525ea0d1f
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:33:19 -0800

    correct my indentation

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,7 @@ namespace JsonApiCoreBooksSample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, AppDbContext context)
         {
-                        app.UseJsonApi();
+            app.UseJsonApi();
         }
     }
 }
