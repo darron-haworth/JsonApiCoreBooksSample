9f114bd56dc64fdd379983ec445dd75e0c81ec22
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:32:58 -0800

    Add middleware and services

--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JsonApiCoreBooksSample.Domain;
+using JsonApiCoreBooksSample.Repository;
+using JsonApiDotNetCore.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,18 +27,28 @@ namespace JsonApiCoreBooksSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            { // use whatever provider you want, this is just an example
+                options.UseSqlite("Data Source=JsonApiCoreSampleData.db");
+            }, ServiceLifetime.Transient);
+
+            // add jsonapi dotnet core
+            services.AddJsonApi<AppDbContext>(
+                options =>
+                {
+                    options.RelativeLinks = false;
+                    options.AllowClientGeneratedIds = true;
+                    options.IncludeTotalRecordCount = true;
+                }
+
+                );
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, AppDbContext context)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
+                        app.UseJsonApi();
         }
     }
 }
