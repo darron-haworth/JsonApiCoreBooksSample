f7f9881eee955fa608070dd8c4d41d99fc2eeded
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:19:17 -0800

    Add new BooksController that imlements JsonApiController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonApiCoreBooksSample.Domain;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JsonApiCoreBooksSample.Controllers
+{
+    public class BooksController : JsonApiController<Book>
+    {
+        public BooksController(
+        IJsonApiContext jsonApiContext,
+        IResourceService<Book>
+        resourceService,
+        ILoggerFactory loggerFactory)
+    : base(jsonApiContext, resourceService, loggerFactory)
+        { }
+    }
+}
