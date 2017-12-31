68bf6c90b44bbbe3a451abf13eb6e266b124492d
darron-haworth <darron.haworth@gmail.com>
2017-12-31 10:06:20 -0800

    Adding Author model POCO

---
+++ Domain/Author.cs
@@ -0,0 +1,18 @@
+﻿using JsonApiDotNetCore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace JsonApiCoreBooksSample.Domain
+{
+    public class Author : Identifiable
+    {
+        [Attr("FirstName")]
+        public string FirstName { get; set; }
+        [Attr("LastName")]
+        public string LastName { get; set; }
+
+        [Attr("Books")]
+        [HasMany("Books")]
+        public List<Book> AuthorBooks { get; set; }
+    }
+}
